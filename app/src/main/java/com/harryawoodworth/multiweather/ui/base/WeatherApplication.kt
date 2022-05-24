264881666ea1df7c3b07817e3d3cb0791305e4f5
Harry Woodworth <harryawoodworth@gmail.com>
2022-05-24 11:55:46 -0400

    Add Application class

---
+++ app/src/main/java/com/harryawoodworth/multiweather/ui/base/WeatherApplication.kt
@@ -0,0 +1,9 @@
+package com.harryawoodworth.multiweather.ui.base
+
+import android.app.Application
+
+/**
+ * Application class annotated with @HiltAndroidApp so that it can serve as the application-level
+ * dependency container.
+ */
+class WeatherApplication : Application()
